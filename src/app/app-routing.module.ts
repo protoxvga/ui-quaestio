961474f1a035a4ad4b1c7a30b7e040bf5f203b10
Kattalin Ocariz <kattalin.ocariz@student.griffith.ie>
2023-05-02 21:31:45 +0100

    [UPDATE] updated app.modules and app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { CreateQuestionComponent } from "./create-question/create-question.component";
+import { QuestionPageComponent } from "./question-page/question-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo:"register", pathMatch:"full" },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'login', component: LoginPageComponent},
-  { path: 'createQuestion', component: CreateQuestionComponent }
+  { path: 'createQuestion', component: CreateQuestionComponent },
+  { path: 'question', component: QuestionPageComponent }
 ];
 
 @NgModule({
