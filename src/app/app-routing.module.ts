e5a4062ccbc0cfb2294a679f6ade142c68822818
Kattalin Ocariz <kattalin.ocariz@student.griffith.ie>
2023-05-05 12:19:30 +0100

    [UPDATE] updated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,9 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { CreateQuestionComponent } from "./create-question/create-question.component";
 import { QuestionPageComponent } from "./question-page/question-page.component";
-import {AnsweredPageComponent} from "./answered-page/answered-page.component";
-import {AllQuestionsPageComponent} from "./all-questions-page/all-questions-page.component";
+import { AnsweredPageComponent } from "./answered-page/answered-page.component";
+import { AllQuestionsPageComponent } from "./all-questions-page/all-questions-page.component";
+import { SearchResultComponent } from "./search-result/search-result.component";
 
 const routes: Routes = [
   { path: "", redirectTo:"home", pathMatch:"full" },
@@ -17,7 +18,8 @@ const routes: Routes = [
   { path: 'createQuestion', component: CreateQuestionComponent },
   { path: 'question/:id', component: QuestionPageComponent },
   { path: 'answered', component: AnsweredPageComponent },
-  { path: 'questions', component: AllQuestionsPageComponent }
+  { path: 'questions', component: AllQuestionsPageComponent },
+  { path: 'searchResult/:type/:term', component: SearchResultComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { QuestionPageComponent } from './question-page/question-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AnsweredPageComponent } from './answered-page/answered-page.component';
 import { AllQuestionsPageComponent } from './all-questions-page/all-questions-page.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { AllQuestionsPageComponent } from './all-questions-page/all-questions-pa
     NavBarComponent,
     AnsweredPageComponent,
     AllQuestionsPageComponent,
+    SearchResultComponent,
   ],
   imports: [
     BrowserModule,
